cef4a57b9795a90c647ee7060ed5d275f79aa267
Michael Quinlan <mquinlan@sportslabs.com>
2013-11-22 09:39:05 -0700

    master: Removing class files and adding new activity

--- src/com/quinlan/placering/PlaceRingMain.java
+++ src/com/quinlan/placering/PlaceRingMain.java
@@ -1,8 +1,11 @@
 package com.quinlan.placering;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class PlaceRingMain extends Activity {
 
@@ -18,6 +21,16 @@ public class PlaceRingMain extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.place_ring_main, menu);
         return true;
+    }
+    
+    public void sendMessage(View view){
+    	Intent intent = new Intent(this, AddLocationActivity.class);
+
+    	EditText editText = (EditText)this.findViewById(R.id.places_edit_text);
+    	String message = editText.getText().toString();
+
+    	intent.putExtra("some extra string", message);
+    	this.startActivity(intent);
     }
     
 }
